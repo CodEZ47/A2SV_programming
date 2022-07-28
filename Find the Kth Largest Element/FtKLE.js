845882835e1a2aa3f1700a188a8c266abc32e197
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2022-07-28 04:00:37 +0300

    Create FtKLE.js

---
+++ Find the Kth Largest Element/FtKLE.js
@@ -0,0 +1,17 @@
+var kthLargestNumber = function(nums, k) {
+     k = nums.length-k;
+     nums.sort((a,b)=>{
+         var n1 = BigInt(a);
+         var n2 = BigInt(b);
+         if(n1>n2)
+         {
+            return 1;
+         }
+         else if(n1<n2)
+         {
+            return -1;
+         }
+         return 0;
+     });
+    return nums[k];
+};
