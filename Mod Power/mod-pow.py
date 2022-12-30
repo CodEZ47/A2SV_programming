7325ccda812b188785d2f2397b98f10246879837
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2022-12-30 15:30:27 +0300

    Create mod-pow.py

---
+++ Mod Power/mod-pow.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+def calculator(n1,n2,n3):
+    print(pow(n1,n2))
+    print(pow(n1,n2,n3))
+    
+
+calculator(num1, num2, num3)
