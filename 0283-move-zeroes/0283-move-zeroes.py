fffe122f40e5db889fa94ada61e38f6708addacf
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-01-12 12:31:18 +0300

    Time: 197 ms (63.06%), Space: 15.7 MB (14.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        curr = 0;
-        nxt = curr + 1;
-        temp = 0;
+        ptr_fir = 0
+        ptr_sec = 1
         nums_len = len(nums)
-
-        while nxt < nums_len:
-            while curr < nums_len and nums[curr] != 0:
-                curr += 1
-                nxt = curr+1
-
-            if nxt < nums_len and nums[nxt] != 0:
-                temp = nums[nxt]
-                nums[nxt] = nums[curr]
-                nums[curr] = temp
-                curr += 1
-                nxt += 1
+        
+        while ptr_sec < nums_len:
+            if nums[ptr_fir] == 0:
+                if nums[ptr_sec] != 0:
+                    nums[ptr_fir], nums[ptr_sec] = nums[ptr_sec], nums[ptr_fir]
+                else:
+                    ptr_sec += 1
+                    continue
             else:
-                nxt += 1
-
-        return nums;
+                ptr_fir += 1
+                ptr_sec += 1
+                
+        return nums
