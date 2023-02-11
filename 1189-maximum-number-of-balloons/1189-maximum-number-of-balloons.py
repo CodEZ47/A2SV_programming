7cac085b4712500db1e3a152fe8fae35a6ae1d94
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-02-11 16:00:23 +0300

    Time: 35 ms (69.48%), Space: 14 MB (7.23%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,15 +1,24 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        t_dict = defaultdict(int)
-        
+        # so we need to count how many b a ll oo n for each character
+        # condition will be, minimum of (num of b, num of a, num of l/2, num of o/2, num of n/2)
+        # hash map of all characters. Another approach is array of 26 and check array since it is small
+        # occurance = defaultdict(int)
+        # for ch in text:
+        #     occurance[ch] += 1
+        # numb = occurance['b']
+        # numa = occurance['a']
+        # numl = occurance['l']//2
+        # numo = occurance['o']//2
+        # numn = occurance['n']
+        # return min(numb, numa, numl, numo, numn)
+        occurance = defaultdict(int)
+        for ch in 'balon':
+            occurance[ch] = 0
         for ch in text:
-            if ch in "balon":
-                t_dict[ch] += 1
-        
-        t_dict['l'] //= 2
-        t_dict['o'] //= 2 
-        
-        if len(t_dict.values()) < 5:
-            return 0
-        return min(t_dict.values())
+            if ch in 'balon':
+                occurance[ch]+= 1
+        occurance['l'] //= 2
+        occurance['o'] //= 2
+        return min(occurance.values())
             
