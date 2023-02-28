3f8b4f96e261f8fcf51ad093ea574d75774166ab
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-03-01 00:44:08 +0300

    Time: 522 ms (58.61%), Space: 18 MB (55.29%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,49 +1,20 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        if len(arr) == 1:
-            return 1
-        if len(arr) == 2 and arr[0] != arr[1]:
-            return 2
-        if len(set(arr)) == 1:
-            return 1
+        inc = 1
+        dec = 1
+        ans = 1
         
-        
-        if arr[0] > arr[1]:
-            flag = False
-        else:
-            flag = True
-        
-        ctr = 1
-        maxi = 0
-        i= 1
-        while i < len(arr)-1:
-            if not flag and arr[i] < arr[i+1]:
-                ctr += 1
-                flag = True
-            elif flag and arr[i] > arr[i+1]:
-                ctr += 1
-                flag = False
-            elif arr[i] == arr[i+1]:
-                while i < len(arr)-1 and arr[i] == arr[i+1]:
-                    i += 1
-                    
-                if i < len(arr)-1 and arr[i] > arr[i+1]:
-                    flag = False
-                else:
-                    flag = True
-                    
-                maxi = max(maxi, ctr)
-                ctr = 1
+        for i in range(1,len(arr)):
+            if arr[i] < arr[i-1]:
+                dec = inc + 1
+                inc = 1
+            elif arr[i] > arr[i-1]:
+                inc = dec + 1
+                dec = 1
             else:
-                if i < len(arr)-1 and arr[i] > arr[i+1]:
-                    flag = False
-                else:
-                    flag = True
-                    
-                maxi = max(maxi, ctr)
-                ctr = 1
-            i += 1
+                inc = 1
+                dec = 1
+            
+            ans = max(ans, max(inc,dec))
         
-        maxi = max(maxi, ctr) + 1
-        
-        return maxi
+        return ans
