1994ba98ece3d04bb347179a6805728f581a462e
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-02-28 10:57:15 +0300

    Time: 711 ms (28.72%), Space: 13.7 MB (99.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,4 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
         if n < 2: return n        
-        return self.fib(n-1) + self.fib(n-2)
+        ans = self.fib(n-1) + self.fib(n-2)
+        return ans
