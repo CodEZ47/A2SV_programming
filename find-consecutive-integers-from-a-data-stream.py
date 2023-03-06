89e9b81cd9b7f3feea9e96ee428ecfb947b27f43
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-03-06 11:34:31 +0300

    added find-consecutive-integers-from-a-data-stream.python

--- find-consecutive-integers-from-a-data-stream.py
+++ find-consecutive-integers-from-a-data-stream.py
@@ -1,22 +1,30 @@
 class DataStream:
 
     def __init__(self, value: int, k: int):
-        self.count=k
-        self.value=value
-        self.temp=0
-        
+        self.stream = [value, k]
+        self.dict = defaultdict(int)
+        self.q = deque()
+        return
 
     def consec(self, num: int) -> bool:
-        if num == self.value:
-            self.temp+=1
-        else:
-            self.temp=0
-        if self.temp>=self.count :
-            return True
-        else:
+        if len(self.q) < self.stream[1]:
+            self.q.append(num)
+            # print(self.q)
+            self.dict[num] += 1
+            if self.dict[self.stream[0]] == self.stream[1]:
+                return True
             return False
         
+        self.dict[self.q[0]] -= 1
+        self.q.popleft()
+        self.q.append(num)
+        self.dict[num] += 1
+
+        if self.dict[self.stream[0]] == self.stream[1]:
+            return True
+        return False
         
+
 
 
 # Your DataStream object will be instantiated and called as such:
