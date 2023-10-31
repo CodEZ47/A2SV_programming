caeac6dd149f0a71f0fdf58892eb5339e1d8c04b
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2023-10-31 23:54:12 +0300

    Added solution for Sum of Left Leaves

---
+++ aftercamp/sum-of-left-leaves.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        sum_left = 0
+
+        def dfs(node):
+            nonlocal sum_left
+            if not node:
+                return 
+            
+            if node.left and not node.left.left and not node.left.right:
+                sum_left += node.left.val
+            dfs(node.left)
+            dfs(node.right)
+
+        
+        dfs(root)
+
+        return sum_left
